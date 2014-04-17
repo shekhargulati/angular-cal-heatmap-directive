2e096d98814c139cdeca12d39214e5478cd97d22
Pedro Narciso García Revington <p.revington@gmail.com>
2014-04-17 20:30:51 +0200

    full config

--- app/scripts/calHeatmap.js
+++ app/scripts/calHeatmap.js
@@ -1,30 +1,31 @@
 'use strict';
-
-angular.module('calHeatmap',[])
-    .directive('calHeatmap', function () {
-        function link(scope, el) {
-            var config = scope.config;
-            var elemenent = el[0];
-            var cal = new CalHeatMap();
-            cal.init({
-                itemSelector: elemenent,
-                domain: !config ? 'month': config.domain ? config.domain : 'month',
-                subDomain: !config ? 'day': config.subDomain ? config.subDomain : 'day',
-                subDomainTextFormat: !config ? '%d' : config.subDomainTextFormat ? config.subDomainTextFormat : '%d',
-                data: !config ? '' : config.data ? config.data : '',
-                start: !config ? new Date() : config.start ? config.start : new Date(),
-                cellSize: !config ? 25 : config.cellSize ? config.cellSize : 25,
-                range: !config ? 3 : config.range ? config.range : 3,
-                domainGutter: !config ? 10 : config.domainGutter ? config.domainGutter : 10,
-                legend: !config ? [2,4,6,8,10] : config.legend ? config.legend : [2, 4, 6, 8, 10],
-                itemName: !config ? 'item' : config.itemName ? config.itemName : 'item'
-              });
-          }
-
-        return {
-            template: '<div id="cal-heatmap" config="config"></div>',
-            restrict: 'E',
-            link: link,
-            scope: { config: '=' }
-          };
-      });
+angular.module('calHeatmap', []).directive('calHeatmap', function () {
+    function link(scope, el) {
+        var config = scope.config || {};
+        var element = el[0];
+        var cal = new CalHeatMap();
+        var defaults = {
+            itemSelector: element,
+            domain: 'month',
+            subDomain: 'day',
+            subDomainTextFormat: '%d',
+            data: '',
+            start: new Date(),
+            cellSize: 25,
+            range: 3,
+            domainGutter: 10,
+            legend: [2, 4, 6, 8, 10],
+            itemName: 'item'
+        };
+        angular.extend(defaults, config);
+        cal.init(defaults);
+    }
+    return {
+        template: '<div id="cal-heatmap" config="config"></div>',
+        restrict: 'E',
+        link: link,
+        scope: {
+            config: '='
+        }
+    };
+});
