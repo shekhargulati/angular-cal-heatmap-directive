25e804441ef8f67d5add6fb29aa026a915c65a7d
Roman Mahrer <mahrer@chello.at>
2016-11-21 17:31:43 +0100

    legend settings on init, scope watching of data

--- app/scripts/calHeatmap.js
+++ app/scripts/calHeatmap.js
@@ -1,31 +1,52 @@
 'use strict';
-angular.module('calHeatmap', []).directive('calHeatmap', function () {
-    function link(scope, el) {
-        var config = scope.config || {};
-        var element = el[0];
-        var cal = new CalHeatMap();
-        var defaults = {
-            itemSelector: element,
-            domain: 'month',
-            subDomain: 'day',
-            subDomainTextFormat: '%d',
-            data: '',
-            start: new Date(),
-            cellSize: 25,
-            range: 3,
-            domainGutter: 10,
-            legend: [2, 4, 6, 8, 10],
-            itemName: 'item'
-        };
-        angular.extend(defaults, config);
-        cal.init(defaults);
-    }
-    return {
-        template: '<div class="cal-heatmap" config="config"></div>',
-        restrict: 'E',
-        link: link,
-        scope: {
-            config: '='
+
+angular.module('calHeatmap', [])
+    .directive('calHeatmap', function() {
+        function link(scope, el) {
+            var config = scope.config;
+            var element = el[0];
+            var cal = new CalHeatMap();
+            cal.init({
+                itemSelector: element,
+                domain: !config ? 'month' : config.domain ? config.domain : 'month',
+                subDomain: !config ? 'day' : config.subDomain ? config.subDomain : 'day',
+                subDomainTextFormat: !config ? '%d' : config.subDomainTextFormat ? config.subDomainTextFormat : '%d',
+                data: !config ? '' : config.data ? config.data : '',
+                start: !config ? new Date() : config.start ? config.start : new Date(),
+                cellSize: !config ? 25 : config.cellSize ? config.cellSize : 25,
+                range: !config ? 3 : config.range ? config.range : 3,
+                domainGutter: !config ? 10 : config.domainGutter ? config.domainGutter : 10,
+                legend: !config ? [2, 4, 6, 8, 10] : config.legend ? config.legend : [2, 4, 6, 8, 10],
+                displayLegend: config.displayLegend || true,
+                legendCellSize: config.legendCellSize || 10,
+                legendCellPadding: config.legendCellPadding || 2,
+                legendMargin: config.legendMargin || [10, 0, 0, 0],
+                legendVerticalPosition: config.legendVerticalPosition || 'bottom',
+                legendHorizontalPosition: config.legendHorizontalPosition || 'left',
+                legendOrientation: config.legendOrientation || 'horizontal',
+                legendColors: config.legendColors || null,
+                itemName: !config ? 'item' : config.itemName ? config.itemName : 'item',
+            });
+
+            scope.$watch("config.displayLegend", function(newValue, oldValue) {
+                if (newValue) {
+                    cal.setLegend();
+                } else {
+                    cal.removeLegend();
+                }
+            }, true);
+
+            scope.$watch("config.data", function(newValue, oldValue) {
+                cal.update(newValue);
+            })
         }
-    };
-});
+
+        return {
+            template: '<div id="cal-heatmap" config="config"></div>',
+            restrict: 'E',
+            link: link,
+            scope: {
+                config: '='
+            }
+        };
+    });
